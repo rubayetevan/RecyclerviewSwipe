aaf1a76dd42bce275d45c9e6a3445739e3251757
Md. Shams -E- Tabriz <0000000000000000000000000000000000000000..0.111111111100000001111>
2016-05-17 13:47:41 +0600

    Initial commit

---
+++ app/src/main/java/com/bdjobs/recyclerviewswipe/MainActivity.java
@@ -0,0 +1,100 @@
+package com.bdjobs.recyclerviewswipe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.daimajia.swipe.util.Attributes;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private ArrayList<Student> mDataSet;
+
+    private Toolbar toolbar;
+
+    private TextView tvEmptyView;
+    private RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvEmptyView = (TextView) findViewById(R.id.empty_view);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // Layout Managers:
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Item Decorator:
+       mRecyclerView.addItemDecoration(new DividerItemDecoration(getResources().getDrawable(R.drawable.divider)));
+         //mRecyclerView.setItemAnimator(new FadeInLeftAnimator());
+
+
+        mDataSet = new ArrayList<Student>();
+
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setTitle("Android Students");
+
+        }
+
+        loadData();
+
+        if (mDataSet.isEmpty()) {
+            mRecyclerView.setVisibility(View.GONE);
+            tvEmptyView.setVisibility(View.VISIBLE);
+
+        } else {
+            mRecyclerView.setVisibility(View.VISIBLE);
+            tvEmptyView.setVisibility(View.GONE);
+        }
+
+
+        // Creating Adapter object
+        SwipeRecyclerViewAdapter mAdapter = new SwipeRecyclerViewAdapter(this, mDataSet);
+
+
+        // Setting Mode to Single to reveal bottom View for one item in List
+        // Setting Mode to Mutliple to reveal bottom Views for multile items in List
+        ((SwipeRecyclerViewAdapter) mAdapter).setMode(Attributes.Mode.Single);
+
+        mRecyclerView.setAdapter(mAdapter);
+
+        /* Scroll Listeners */
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                Log.e("RecyclerView", "onScrollStateChanged");
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+            }
+        });
+    }
+
+
+    // load initial data
+    public void loadData() {
+
+        for (int i = 0; i <= 20; i++) {
+            mDataSet.add(new Student("Student " + i, "androidstudent" + i + "@gmail.com"));
+
+        }
+
+
+    }
+
+}
+
